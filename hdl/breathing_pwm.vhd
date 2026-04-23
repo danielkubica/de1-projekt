3354cc3ec4c206fc693edbf5d90eb0ee6f39756e
Daniel Kubica <daniekubica@gmail.com>
2026-04-23 10:56:15 +0200

    modified the breathing_pwm entity for smoother transition

--- hdl/breathing_pwm.vhd
+++ hdl/breathing_pwm.vhd
@@ -1,4 +1,4 @@
--- Modul generujuci "breathing" efekt PWM signal, ktoreho dlzka zavisi od inhale_time
+-- -- Modul generujuci "breathing" efekt PWM signal, ktoreho dlzka zavisi od inhale_time
 
 library ieee;
 use ieee.std_logic_1164.all;
@@ -6,68 +6,85 @@ use ieee.numeric_std.all;
 
 entity breathing_pwm is
     port (
-        clk                     : in  std_logic;
-        inhale_time             : in unsigned(2 downto 0); -- 3bit, maximalny inhale_time 8 sekund     
-        pwm_signal              : out std_logic := '0'
+        clk          : in  std_logic;
+        inhale_time  : in  unsigned(2 downto 0); 
+        pwm_signal   : out std_logic := '0'
     );
 end entity breathing_pwm;
 
 architecture behavioral of breathing_pwm is
-    constant CLOCK_FREQ          : integer := 10_000; -- treba zmenit na 100_000_000 pre FPGAcko
-    constant TOTAL_LIGHT_LEVELS  : integer := 64; 
+    -- For Simulation: 10_000. For FPGA: 100_000_000
+    constant CLOCK_FREQ         : integer := 10_000; 
+    constant PWM_RESOLUTION     : integer := 100;    
 
-    signal cycles                : integer := 0;
-    signal light_level_period    : integer := 0; -- 1_562_500 pre 64 levlov a 1s nadych
-    signal current_light_level   : unsigned(5 downto 0) := (others => '0');
-    signal count                 : integer := 0;
-    signal inhale                : std_logic := '1';
+    -- PWM signals
+    signal pwm_cnt              : integer range 0 to PWM_RESOLUTION - 1 := 0;
+    
+    -- Breathing signals
+    signal breath_tick_cnt      : integer := 0;
+    signal current_duty         : integer range 0 to PWM_RESOLUTION := 0;
+    signal inhale               : std_logic := '1';
+
 begin
 
-    p_count : process(clk)
-        variable v_cycles : integer;
-    begin        
+    -- 1. FAST PWM GENERATOR
+    -- This runs at CLOCK_FREQ / 100. 
+    -- With 10kHz clock, PWM freq = 100Hz (perfectly smooth).
+    p_pwm : process(clk)
+    begin
         if rising_edge(clk) then
-            v_cycles := to_integer(inhale_time) * CLOCK_FREQ;
-            cycles <= v_cycles;
-            light_level_period <= v_cycles / TOTAL_LIGHT_LEVELS;
-
-            if inhale_time = b"000" then
-                count <= 0;
-                pwm_signal <= '0';
-                current_light_level <= (others => '0');
+            -- Counter always loops 0-99
+            if pwm_cnt < PWM_RESOLUTION - 1 then
+                pwm_cnt <= pwm_cnt + 1;
             else
+                pwm_cnt <= 0;
+            end if;
 
-                -- 1. Main counter for the duration of one brightness level
-                if count < light_level_period - 1 then
-                    count <= count + 1;
+            -- PWM Output logic
+            if pwm_cnt < current_duty then
+                pwm_signal <= '1';
+            else
+                pwm_signal <= '0';
+            end if;
+        end if;
+    end process p_pwm;
+
+    -- 2. SLOW DUTY CYCLE UPDATER
+    p_breath : process(clk)
+        variable v_total_cycles_per_half_breath : integer;
+        variable v_cycles_per_step              : integer;
+    begin
+        if rising_edge(clk) then
+            if inhale_time = "000" then
+                current_duty <= 0;
+                breath_tick_cnt <= 0;
+            else
+                -- Math to figure out how many clock cycles to wait before changing 1% of duty
+                v_total_cycles_per_half_breath := to_integer(inhale_time) * CLOCK_FREQ;
+                v_cycles_per_step              := v_total_cycles_per_half_breath / PWM_RESOLUTION;
+
+                if breath_tick_cnt < v_cycles_per_step then
+                    breath_tick_cnt <= breath_tick_cnt + 1;
                 else
-                    count <= 0;
-                    -- 2. Update brightness level
+                    breath_tick_cnt <= 0;
+                    
+                    -- Change the brightness level
                     if inhale = '1' then
-                        if current_light_level = 63 then
+                        if current_duty >= PWM_RESOLUTION then
                             inhale <= '0';
                         else
-                            current_light_level <= current_light_level + 1;
+                            current_duty <= current_duty + 1;
                         end if;
                     else
-                        if current_light_level = 0 then
+                        if current_duty <= 0 then
                             inhale <= '1';
                         else
-                            current_light_level <= current_light_level - 1;
+                            current_duty <= current_duty - 1;
                         end if;
                     end if;
                 end if;
-                
-                -- Sled operacii v tomto riadku je podstatny! Pre simulaciu s nizsim CLOCK_FREQ najprv musime nasobit
-                -- (current_light_level * LIGHT_LEVEL_PERIOD) a az potom vydelit TOTAL_LIGHT_LEVELS.
-                -- inac pri napr. CLOCK_FREQ = 1000, by to davalo furt nulu a pwm_signal by bol furt nula.
-                if count < ((to_integer(current_light_level) * light_level_period) / TOTAL_LIGHT_LEVELS) then
-                    pwm_signal <= '1';
-                else
-                    pwm_signal <= '0';
-                end if;    
             end if;
         end if;
-    end process p_count;
+    end process p_breath;
 
 end architecture;
